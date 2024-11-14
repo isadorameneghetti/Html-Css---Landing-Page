9a55a8565fb91ff268713c58d7eaebfd22775cab
isadorameneghetti <isadorammeneghetti@gmail.com>
2024-11-14 11:00:25 -0300

    error

--- script.js
+++
@@ -1,8 +0,0 @@
-const menuIcon = document.querySelector('#menu-icon');
-const navigation = document.querySelector('.navigation');
-const navbg = document.querySelector('.nav-bg');
-menuIcon.addEventListener('click', () => {
-    menuIcon.classList.toggle('bx-x');
-    navigation.classList.toggle('active');
-    navbg.classList.toggle('active');
-});
